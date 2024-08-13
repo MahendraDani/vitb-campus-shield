d038142adbd8480f6a6c4dd5f0c0ab75b3b6668d
Anushree <anushreejaiswal.srn@gmail.com>
2024-08-13 15:35:07 +0530

    updated styles

--- components/support.tsx
+++ components/support.tsx
@@ -13,44 +13,42 @@ export const Support = () => {
                 Some valuable external resources where you can learn more about
                 anti-ragging measures, support, and legal frameworks.
             </p>
-            <Fade cascade triggerOnce className="w-full">
-                <Link
-                    href="https://www.antiragging.in/"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    1. National Anti-Ragging Helpline
-                </Link>
-                <Link
-                    href="https://www.ugc.gov.in/pdfnews/8408691_Step-by-step-guide-on-How-to-fill-Online-Anti-Ragging-Undertaking.pdf"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    2. University Grants Commission (UGC) Anti-Ragging Portal
-                </Link>
-                <Link
-                    href="https://www.legalserviceindia.com/"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    3. Legal Service India
-                </Link>
-                <Link
-                    href="https://www.education.gov.in/major-initiatives"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    4. Ministry of Education, Government of India
-                </Link>
-                <Link
-                    href="https://nhrc.nic.in/"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    5. National Human Rights Commission (NHRC) of India
-                </Link>
-                <Link
-                    href="https://no2ragging.org/"
-                    className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
-                >
-                    6. Save Education, Save Society (SESS) Anti-Ragging Campaign
-                </Link>
-            </Fade>
+            <Link
+                href="https://www.antiragging.in/"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                1. National Anti-Ragging Helpline
+            </Link>
+            <Link
+                href="https://www.ugc.gov.in/pdfnews/8408691_Step-by-step-guide-on-How-to-fill-Online-Anti-Ragging-Undertaking.pdf"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                2. University Grants Commission (UGC) Anti-Ragging Portal
+            </Link>
+            <Link
+                href="https://www.legalserviceindia.com/"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                3. Legal Service India
+            </Link>
+            <Link
+                href="https://www.education.gov.in/major-initiatives"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                4. Ministry of Education, Government of India
+            </Link>
+            <Link
+                href="https://nhrc.nic.in/"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                5. National Human Rights Commission (NHRC) of India
+            </Link>
+            <Link
+                href="https://no2ragging.org/"
+                className="border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] w-full py-2 px-3 rounded-xl text-2xl font-semibold hover:bg-[#003a6b] hover:text-white transition-colors"
+            >
+                6. Save Education, Save Society (SESS) Anti-Ragging Campaign
+            </Link>
         </div>
     );
 };

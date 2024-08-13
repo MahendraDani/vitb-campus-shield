539dee2161b4283bcff3ad8764c23959d0b8c54a
Anushree <anushreejaiswal.srn@gmail.com>
2024-08-13 16:50:24 +0530

    added animations

--- components/checkstatus.tsx
+++ components/checkstatus.tsx
@@ -6,42 +6,51 @@ import { Button } from "./ui/button";
 import { FormEvent } from "react";
 
 export const CheckStatus = () => {
-  const hanldeReportStatus = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const id = formData.get("id");
-    const res = await fetch(`/api/report/${id}`);
-    const response = await res.json();
-    console.log(response);
-    alert(response.data.report.status);
-  };
-  return (
-    <div className="w-full min-h-screen text-[#003A6B] px-10 py-8 ">
-      <div className="bg-[#89CFF1] flex flex-col gap-10 items-center rounded-xl p-8">
-        <CircleCheckBig width={72} height={72} color="#003a6b" />
-        <Fade className="text-5xl font-bold text-center">
-          Check your Complaint Status
-        </Fade>
+    const hanldeReportStatus = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const id = formData.get("id");
+        const res = await fetch(`/api/report/${id}`);
+        const response = await res.json();
+        console.log(response);
+        alert(response.data.report.status);
+    };
+    return (
+        <div className="w-full min-h-screen text-[#003A6B] px-10 py-8 ">
+            <div className="bg-[#89CFF1] flex flex-col gap-10 items-center rounded-xl p-8">
+                <CircleCheckBig width={72} height={72} color="#003a6b" />
+                <Fade className="text-5xl font-bold text-center">
+                    Check your Complaint Status
+                </Fade>
 
-        <div className="bg-white w-full rounded-lg flex justify-center gap-10 md:flex-wrap">
-          <div className="p-4 flex flex-col justify-center gap-5">
-            <p className="text-xl font-medium">
-              Complaints registered after{" "}
-              <span className="font-bold text-[#">After April 2024</span>
-            </p>
-            <form onSubmit={hanldeReportStatus}>
-              <Input type="text" placeholder="Complaint Number" name="id" />
-              <Button variant={"destructive"} type="submit">
-                Check Status
-              </Button>
-            </form>
-          </div>
-          <img
-            src="https://www.antiragging.in/assets/img/index/complaint-status.png"
-            className="w-auto h-[400px]"
-          />
+                <div className="bg-white w-full rounded-lg flex justify-center gap-10 md:flex-wrap">
+                    <div className="p-4 flex flex-col justify-center gap-5">
+                        <p className="text-xl font-medium">
+                            Complaints registered after{" "}
+                            <span className="font-bold text-[#">
+                                After April 2024
+                            </span>
+                        </p>
+                        <form
+                            onSubmit={hanldeReportStatus}
+                            className="w-full flex flex-col justify-center gap-5"
+                        >
+                            <Input
+                                type="text"
+                                placeholder="Complaint Number"
+                                name="id"
+                            />
+                            <Button variant={"destructive"} type="submit">
+                                Check Status
+                            </Button>
+                        </form>
+                    </div>
+                    <img
+                        src="https://www.antiragging.in/assets/img/index/complaint-status.png"
+                        className="w-auto h-[400px]"
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- components/file-complaint.tsx
+++ components/file-complaint.tsx
@@ -9,63 +9,67 @@ export const FileComplaint = () => {
             <Fade className="text-5xl font-bold text-center">
                 Fill your Complaint
             </Fade>
-            <div className="flex gap-14 md:flex-wrap">
-                <Link
-                    href={
-                        "https://www.antiragging.in/affidavit_registration_disclaimer.html"
-                    }
-                    className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
-                >
-                    <img
-                        src="https://www.antiragging.in/assets/img/index/register.webp"
-                        className="h-[250px] rounded-t-xl"
-                    />
-                    <p className="rounded-b-xl text-xl font-semibold text-center">
-                        Fill Undertaking
-                    </p>
-                </Link>
+            <Fade triggerOnce>
+                <div className="flex gap-14 md:flex-wrap">
+                    <Link
+                        href={
+                            "https://www.antiragging.in/affidavit_registration_disclaimer.html"
+                        }
+                        className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
+                    >
+                        <img
+                            src="https://www.antiragging.in/assets/img/index/register.webp"
+                            className="h-[250px] rounded-t-xl"
+                        />
+                        <p className="rounded-b-xl text-xl font-semibold text-center">
+                            Fill Undertaking
+                        </p>
+                    </Link>
 
-                <Link
-                    href={
-                        "https://www.antiragging.in/compliance_desclaimer.html"
-                    }
-                    className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
-                >
-                    <img
-                        src="https://www.antiragging.in/assets/img/index/compliance.webp"
-                        className="h-[250px] rounded-t-xl"
-                    />
-                    <p className="rounded-b-xl text-xl font-semibold text-center">
-                        Fill Compliance
-                    </p>
-                </Link>
+                    <Link
+                        href={
+                            "https://www.antiragging.in/compliance_desclaimer.html"
+                        }
+                        className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
+                    >
+                        <img
+                            src="https://www.antiragging.in/assets/img/index/compliance.webp"
+                            className="h-[250px] rounded-t-xl"
+                        />
+                        <p className="rounded-b-xl text-xl font-semibold text-center">
+                            Fill Compliance
+                        </p>
+                    </Link>
 
-                <Link
-                    href={"/file-a-complaint"}
-                    className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
-                >
-                    <img
-                        src="https://www.antiragging.in/assets/img/index/being-ragged.webp"
-                        className="h-[250px] rounded-t-xl"
-                    />
-                    <p className="rounded-b-xl text-xl font-semibold text-center">
-                        Register Complaint
-                    </p>
-                </Link>
+                    <Link
+                        href={"/file-a-complaint"}
+                        className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
+                    >
+                        <img
+                            src="https://www.antiragging.in/assets/img/index/being-ragged.webp"
+                            className="h-[250px] rounded-t-xl"
+                        />
+                        <p className="rounded-b-xl text-xl font-semibold text-center">
+                            Register Complaint
+                        </p>
+                    </Link>
 
-                <Link
-                    href={"https://www.antiragging.in/undertaking_request.php"}
-                    className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
-                >
-                    <img
-                        src="https://www.antiragging.in/assets/img/index/get_document.webp"
-                        className="h-[250px] rounded-t-xl"
-                    />
-                    <p className="rounded-b-xl text-xl font-semibold text-center">
-                        Undertaking Document
-                    </p>
-                </Link>
-            </div>
+                    <Link
+                        href={
+                            "https://www.antiragging.in/undertaking_request.php"
+                        }
+                        className="w-[300px] h-[300px] bg-[#003A6B] pb-2 border-2 border-[#003A6B] outline-2 outline outline-offset-4 outline-[#003A6B] rounded-xl flex flex-col gap-3 text-white hover:bg-white hover:text-[#003a6b] transition-colors"
+                    >
+                        <img
+                            src="https://www.antiragging.in/assets/img/index/get_document.webp"
+                            className="h-[250px] rounded-t-xl"
+                        />
+                        <p className="rounded-b-xl text-xl font-semibold text-center">
+                            Undertaking Document
+                        </p>
+                    </Link>
+                </div>
+            </Fade>
         </div>
     );
 };
